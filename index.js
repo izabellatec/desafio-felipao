a9f8aa4f7fa410b9076a7b680f89bc0845f908c3
izabellatec <izabella_hto18@hotmail.com>
2023-11-18 19:40:10 -0300

    meu desafio da dio

---
+++ index.js
@@ -0,0 +1,23 @@
+let heroi = "homenaranha";
+let xp = parseInt(prompt("Digite a quantidade de experiência do herói: (7003)"));
+let nivel;
+
+if (xp < 1000) {
+    nivel = "ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+    nivel = "bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+    nivel = "prata";
+} else if (xp >= 6001 && xp <= 7000) {  
+    nivel = "ouro";
+} else if (xp >= 7001 && xp <= 8000) {  
+    nivel = "platina";
+} else if (xp >= 8001 && xp <= 9000) {
+    nivel = "ascendente";   
+} else if (xp >= 9001 && xp <= 10000) {
+    nivel = "imortal";
+} else {
+    nivel = "radiante";
+}
+
+console.log("O herói de nome: ${heroi} está no nível de ${nivel}");
